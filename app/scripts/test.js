88e89ef7ba6988e8737b8afd0727dc25a9d80d6b
Johnathon Sanders <outdooricon@gmail.com>
2014-10-18 07:15:24 -0500

    Add reqwest and moment to demonstrate bug

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -1,4 +1,6 @@
 import S from 'string/lib/string';
+import moment from 'moment/moment';
+import reqwest from 'reqwest/reqwest';
 import otherTest from 'scripts/testFolder/otherTest';
 
 var a = 'a';
